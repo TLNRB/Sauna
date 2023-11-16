746bf4fac9bd45e81bbd3dff06882aa7702f9ed5
TLNRB <nortol01@easv365.dk>
2023-11-16 11:48:13 +0100

    base content of index added

---
+++ index.php
@@ -0,0 +1,11 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>SaunaLand</title>
+    </head>
+    <body>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum error, perferendis excepturi rerum pariatur nostrum, consectetur quis officia fuga ipsum officiis optio debitis vero inventore maiores totam laudantium nesciunt voluptates?</p>
+    </body>
+    </html>
