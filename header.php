85641503673d07dc2a8d4b586fb4962b740a12c1
TLNRB <nortol01@easv365.dk>
2023-11-18 12:07:49 +0100

    menu mobile dropdown design

--- header.php
+++ header.php
@@ -7,6 +7,20 @@
   <?php wp_head() ?>
 </head>
 <body>
-  <header>
-    <nav class=""><?php wp_nav_menu( array( 'theme_location' => 'wp_sauna_main_menu', 'depth' => 1 )); ?></nav>
+  <header class="flex items-center p-[1.25rem]">
+    <!-- Logo -->
+    <div class="text-[1.125rem] font-bold">SaunaLand.</div>
+    <!-- Menu Links -->
+    <nav class="hidden"><?php wp_nav_menu( array( 'theme_location' => 'wp_sauna_main_menu', 'depth' => 1 )); ?></nav>
+    <!-- Language Picker -->
+    <div class="ml-auto flex gap-[.375rem] items-center text-[.875rem] font-[500] cursor-pointer">
+      <p>HU</p>
+      <p>EN</p>
+    </div>
+    <!-- Menu Dropdown -->
+    <div class="menu-icon">
+      <span class="menu-icon__line menu-icon__line-left"></span>
+      <span class="menu-icon__line"></span>
+      <span class="menu-icon__line menu-icon__line-right"></span>
+    </div>
   </header>
