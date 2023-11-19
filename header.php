7facebaf7105c4f974a8083225d724aa12941acd
TLNRB <nortol01@easv365.dk>
2023-11-19 18:36:08 +0100

    hero section designed and nav updated

--- header.php
+++ header.php
@@ -7,13 +7,13 @@
   <?php wp_head() ?>
 </head>
 <body>
-  <header class="fixed top-0 right-0 left-0 flex items-center pt-[1.25rem] px-[1rem] z-[10] sm:px-[1.5rem] md:px-[2rem] lg:py-[2rem] lg:px-[4rem] xxl:px-[6rem] xxxl:px-[8rem]">
+  <header class="fixed backdrop-blur-[12px] top-0 right-0 left-0 flex items-center py-[1rem] px-[1rem] z-[10] sm:px-[1.5rem] md:px-[2rem] lg:pt-[1.25rem] lg:px-[4rem] xxl:px-[6rem] xxxl:px-[8rem]">
     <!-- Logo -->
     <div class="text-[1.125rem] font-bold z-[2] xs:text-[1.25rem] sm:text-[1.5rem]">SaunaLand.</div>
     <!-- Menu Links -->
-    <nav id="menu" class="absolute translate-y-[-400px] duration-[.25s] ease-in-out left-0 top-0 right-0 pt-[4rem] pb-[1.5rem] bg-bgNormal text-[1.25rem] font-[500] text-textDark drop-shadow-lg rounded-b-[20px] sm:text-[1.375rem] md:translate-y-0 md:relative md:text-[1rem] md:bg-transparent md:p-0 md:ml-auto xxxxl:text-[1.125rem]"><?php wp_nav_menu( array( 'theme_location' => 'wp_sauna_main_menu', 'depth' => 1 )); ?></nav>
+    <nav id="menu" class="absolute translate-y-[-400px] duration-[.25s] ease-in-out left-0 top-0 right-0 pt-[4rem] pb-[1.5rem] bg-bgNormal text-[1.25rem] font-[500] text-textDark drop-shadow-lg rounded-b-[20px] sm:text-[1.375rem] md:translate-y-0 md:relative md:text-[1rem] md:bg-transparent md:p-0 md:ml-auto "><?php wp_nav_menu( array( 'theme_location' => 'wp_sauna_main_menu', 'depth' => 1 )); ?></nav>
     <!-- Language Picker -->
-    <div class="ml-auto flex gap-[.375rem] items-center text-[.875rem] font-[500] text-textDark cursor-pointer z-[2] md:ml-[3rem] lg:ml-[4rem] xxxxl:text-[1rem]">
+    <div class="ml-auto flex gap-[.375rem] items-center text-[.875rem] font-[500] cursor-pointer z-[2] md:ml-[3rem] lg:ml-[4rem] xxxxl:text-[1rem]">
       <p>HU</p>
       <p>EN</p>
     </div>
--- template-parts/home-template.php
+++ template-parts/home-template.php
@@ -1,14 +1,25 @@
 <?php
 /*
-Template Name: Home Page Template
+Template Name: Main Page Template
 */
 ?>
 
 <?php get_header(); ?>
 
-  <main class="mt-[4rem]" >
+  <main class="" >
     <?php while (have_posts()): the_post(); ?>
-      <section id="home"></section>
+      <section id="home" class="flex flex-col md:flex-row md:justify-center md:gap-[4rem] md:mx-[1rem] md:bg-bgNormal md:pt-[10rem] md:rounded-b-[30px] md:drop-shadow-md lg:gap-[6rem] xl:gap-[8rem] xxl:gap-[10rem] xxl:mx-[2rem] xxxl:rounded-b-[35px] xxxxl:pt-[12rem]">
+        <div class="pt-[7rem] flex flex-col items-center gap-[2rem] pb-[3rem] px-[1rem] bg-bgNormal sm:pt-[8rem] sm:px-[1.5rem] md:pt-[1rem] md:pb-[4rem] md:pl-[3rem] md:pr-0 md:items-start md:bg-transparent md:w-[100%] xl:gap-[2.5rem] xxl:pt-[2rem] xxl:pl-[4rem] xxxl:justify-center xxxl:py-0 xxxl:pl-[6rem] ">
+          <h1 class="w-[250px] text-center text-[2.5rem] font-semibold leading-[3rem] sm:w-[300px] sm:text-[3rem] sm:leading-[3.25rem] md:text-left xl:text-[4rem] xl:w-[400px] xl:leading-[4rem] xxl:w-[500px] xxl:text-[5rem] xxl:leading-[5rem] xxxxl:w-[600px] xxxxl:text-[6rem] xxxxl:leading-[6rem]"><?php the_title("") ?></h1>
+          <hr class="w-[100%] h-[2px] bg-bgSemiDark">
+          <p class="text-bgSemiDark xxxxl:text-[1.25rem]"><?php the_field("hero_description") ?></p>
+          <a href="#contact" class="mt-[1rem] py-[.375rem] px-[1rem] rounded-full border-[1.5px] border-bgSemiDark font-semibold hover:scale-105 duration-[.175s] ease-in-out xxxxl:text-[1.125rem]  xxxxl:px-[1.125rem]">Contact Us</a>
+        </div>
+        <div>
+          <?php $heroImage = get_field("hero_image") ?>
+          <img src="<?php echo $heroImage["url"] ?>" alt="<?php echo $heroImage["alt"] ?>" class="h-[250px] object-cover object-center rounded-b-[20px] drop-shadow-lg xs:h-[375px] sm:h-[525px] sm:rounded-b-[25px] md:h-[100%]  md:rounded-bl-none md:rounded-tl-[30px] md:rounded-br-[30px] md:drop-shadow-none xxxl:rounded-br-[35px] xxxl:rounded-tl-[35px]">
+        </div>
+      </section>
       <section id="about">about</section>
       <section id="services">services</section>
       
